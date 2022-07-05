681353c4453575ba3648dd16e3a0a7002acb99b3
Adelakun Tobiloba <adelakuntobiloba1@gmail.com>
2022-07-05 01:46:10 +0100

    urbn

--- src/assets/images/urbn/index.js
+++ src/assets/images/urbn/index.js
@@ -1,34 +1,22 @@
-import urbn1 from "./urbn1.svg";
-import urbn2 from "./urbn2.svg";
-import urbn3 from "./urbn3.svg";
-import urbn4 from "./urbn4.svg";
-import urbn5 from "./urbn5.svg";
-import urbn6 from "./urbn6.svg";
-import urbn7 from "./urbn7.svg";
-import urbn8 from "./urbn8.svg";
-import urbn9 from "./urbn9.svg";
-import urbn10 from "./urbn10.svg";
-import urbn11 from "./urbn11.svg";
-import urbn12 from "./urbn12.svg";
-import urbn13 from "./urbn13.svg";
-import urbn14 from "./urbn14.svg";
-import urbn15 from "./urbn15.svg";
-import urbn16 from "./urbn16.svg";
-import urbn17 from "./urbn17.svg";
-import urbn18 from "./urbn18.svg";
-import urbn19 from "./urbn19.svg";
-import urbn20 from "./urbn20.svg";
-import urbn21 from "./urbn21.svg";
-import urbn22 from "./urbn22.svg";
-import urbn23 from "./urbn23.svg";
-import urbn24 from "./urbn24.svg";
-import urbn25 from "./urbn25.svg";
-import urbn26 from "./urbn26.svg";
-import urbn27 from "./urbn27.svg";
-import urbn28 from "./urbn28.svg";
-import urbn29 from "./urbn29.svg";
-import urbn30 from "./urbn30.svg";
-import urbn31 from "./urbn31.svg";
+import urbn1 from "./urbn1.png";
+import urbn2 from "./urbn2.png";
+import urbn3 from "./urbn3.png";
+import urbn4 from "./urbn4.png";
+import urbn5 from "./urbn5.png";
+import urbn6 from "./urbn6.png";
+import urbn7 from "./urbn7.png";
+import urbn8 from "./urbn8.png";
+import urbn9 from "./urbn9.png";
+import urbn10 from "./urbn10.png";
+import urbn11 from "./urbn11.png";
+import urbn12 from "./urbn12.png";
+import urbn13 from "./urbn13.png";
+import urbn14 from "./urbn14.png";
+import urbn15 from "./urbn15.png";
+import urbn16 from "./urbn16.png";
+import urbn17 from "./urbn17.png";
+import urbn18 from "./urbn18.png";
+import urbn19 from "./urbn19.png";
 
 
 export{
@@ -50,17 +38,5 @@ export{
   urbn16,
   urbn17,
   urbn18,
-  urbn19,
-  urbn20,
-  urbn21,
-  urbn22,
-  urbn23,
-  urbn24,
-  urbn25,
-  urbn26,
-  urbn27,
-  urbn28,
-  urbn29,
-  urbn30,
-  urbn31
+  urbn19
 }
--- src/pages/Works/Urbn.js
+++ src/pages/Works/Urbn.js
@@ -23,26 +23,14 @@ import {
   urbn17,
   urbn18,
   urbn19,
-  urbn20,
-  urbn21,
-  urbn22,
-  urbn23,
-  urbn24,
-  urbn25,
-  urbn26,
-  urbn27,
-  urbn28,
-  urbn29,
-  urbn30,
-  urbn31,
 } from '../../assets/images/urbn';
 
 export default function Urbn() {
   let components = [ urbn2,
     urbn3, urbn4, urbn5, urbn6, urbn7, urbn8, urbn9,
     urbn10, urbn11, urbn12, urbn13, urbn14, urbn15,
-    urbn16, urbn17, urbn18, urbn19, urbn20, urbn21, urbn22, urbn23, urbn24, urbn25, urbn26, urbn27, urbn28, urbn29, urbn30, urbn31,]
-  for (let i = 2; i < 30; i++) {
+    urbn16, urbn17, urbn18, urbn19]
+  for (let i = 2; i < 19; i++) {
     components.push(<img src={`urbn${i}`} alt="" />)
   }
   return (
@@ -50,15 +38,22 @@ export default function Urbn() {
       title="URBN - Streetwear Fashion Branding"
       banner={urbn1}
       next="Back to Homepage">
-      {/* <div>
-        {[...Array(30)].map((x, i) =>
-          // <img
-          //   src={require(`../../assets/images/urbn/urbn${i + 2}.svg`)}
-          //   alt="" key={i + 2} className="w-full" />
+      <div>
+        {[...Array(8)].map((x, i) =>
+          <img
+            src={require(`../../assets/images/urbn/urbn${i + 2}.png`)}
+            alt="" key={i + 2} className="w-full" />
        
         )}
-      </div> */}
-      <div>
+        <h3 className='mt-8 mb-6 font-bold text-3xl pl-12 '>Use Cases</h3>
+        {[...Array(10)].map((x, i) =>
+          <img
+            src={require(`../../assets/images/urbn/urbn${i + 10}.png`)}
+            alt="" key={i + 10} className="w-full" />
+       
+        )}
+      </div>
+      {/* <div>
         {
           components.map(element => {
             return (
@@ -66,7 +61,7 @@ export default function Urbn() {
             )
           })
         }
-      </div>
+      </div> */}
     </Layout>
   )
 }
